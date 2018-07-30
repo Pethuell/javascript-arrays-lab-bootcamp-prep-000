2105c914a3d6b03808b097a42e69de026ff82351
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-07-30 19:48:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
 
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
